d04f1cfd60d573f7f08d70b758635686968aeda5
Jahongir2109 <jahongirachilov004@gmail.com>
2022-11-29 14:50:54 +0500

    return all

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -12,11 +12,11 @@ public class ReassigningValues {
         first=15;
         second=6;
         third=4;
-        System.err.println(first);
-        System.err.println(second);
-        System.err.println(third);
-        System.err.println(linkToFirst);
-        System.err.println(linkToSecond);
-        System.err.println(linkToThird);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
