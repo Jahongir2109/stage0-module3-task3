387aa4803933c3bf4df08fd5e9fff2256f3c48c5
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-19 10:32:19 +0500

    basic initialization changed

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -6,9 +6,7 @@ public class ReassigningValues {
         int first=1;
         int second=10;
         int third=100;
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
+        System.out.println(first+"\n"+second+"\n"+third);
         int linkToFirst=first;
         int linkToSecond=second;
         int linkToThird=third;
